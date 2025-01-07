f0e49cb7cb2a0ba1aa405d9cd54211f5e413dfcc
gaolei <gaolei@seer-group.com>
2025-01-07 18:44:48 +0800

    add: 增加config 文件

---
+++ config.py
@@ -0,0 +1,88 @@
+
+
+pmodel={
+    "LM":{
+            "className": "LocationMark",
+            "instanceName": "LM1",
+            "pos": {
+                "x": -73.491,
+                "y": -41.442
+            },
+            "property": [
+                {
+                    "key": "spin",
+                    "type": "bool",
+                    "value": "ZmFsc2U=",
+                    "boolValue": False
+                }
+            ],
+            "ignoreDir": True
+        },
+    "PP": {
+            "className": "ParkPoint",
+            "instanceName": "PP2",
+            "pos": {
+                "x": -74.978,
+                "y": -39.854
+            },
+            "property": [
+                {
+                    "key": "spin",
+                    "type": "bool",
+                    "value": "ZmFsc2U=",
+                    "boolValue": False
+                }
+            ],
+            "ignoreDir": True
+        },
+    "AP":{
+            "className": "ActionPoint",
+            "instanceName": "AP3",
+            "pos": {
+                "x": -73.491,
+                "y": -39.854
+            },
+            "property": [
+                {
+                    "key": "spin",
+                    "type": "bool",
+                    "value": "ZmFsc2U=",
+                    "boolValue": False
+                }
+            ],
+            "ignoreDir": True
+        },
+    "CP": {
+            "className": "ChargePoint",
+            "instanceName": "CP4",
+            "pos": {
+                "x": -75.089,
+                "y": -41.221
+            },
+            "property": [
+                {
+                    "key": "spin",
+                    "type": "bool",
+                    "value": "ZmFsc2U=",
+                    "boolValue": False
+                }
+            ],
+            "ignoreDir": True
+        },
+    "SM":{
+            "className": "SwitchMap",
+            "instanceName": "SM5",
+            "pos": {
+                "x": -72.04,
+                "y": -39.854
+            },
+            "property": [
+                {
+                    "key": "spin",
+                    "type": "bool",
+                    "value": "ZmFsc2U=",
+                    "boolValue": False
+                }
+            ]
+        }
+}
